dd92b5c64010e807cca6215627cda0aa1b63c88e
Mikaaa <61538368+khoale38@users.noreply.github.com>
2024-02-23 16:53:37 +0700

    add 103

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,5 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addCollection("delis", function(collectionApi) {
-
     return collectionApi.getFilteredByGlob("demo/deli-*.md");
   });
 };
