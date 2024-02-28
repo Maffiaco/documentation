c3bb6df57309325e76d87bbe0cbba4044b9bbbaf
anvox <thien.an.vo.nguyen@gmail.com>
2024-02-28 14:23:22 +0700

    Improve

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,21 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("demos", function (collectionApi) {
     return collectionApi.getFilteredByGlob("demos/deli-*.md").reverse();
+  });
+  eleventyConfig.addShortcode("sprintTitle", function (sprintContent) {
+    const dom = new JSDOM(sprintContent);
+    return dom.window.document.querySelector("h2").textContent;
+  });
+
+  eleventyConfig.addShortcode("releasedItems", function (sprintContent) {
+    const dom = new JSDOM(sprintContent);
+    return Array.from(dom.window.document.querySelectorAll("h3")).map((h3) => {
+      return h3.textContent;
+    }).filter((releasedItem) => {
+      return releasedItem.includes("🚀")
+    });
   });
 };
--- index.11ty.js
+++ index.11ty.js
@@ -5,11 +5,16 @@ exports.data = function () {
     },
   }
 };
+
+const renderReleasedItems = (items) => {
+  return items.map((item) => (`<li>${item}</li>`)).join("\n");
+}
+
 exports.render = function (data) {
   return `<h1>Our Sprint Track</h1>
   <ul>
     ${data.collections.demos.map((sprint) => {
-    return `<li><a href="${sprint.url}">${sprint.fileSlug}</a></li>`;
+    return `<li><a href="${sprint.url}">${this.sprintTitle(sprint.content)}</a><br /><ul>${renderReleasedItems(this.releasedItems(sprint.content))}</ul></li>`;
   }).join("\n")}
   </ul>`;
 };
