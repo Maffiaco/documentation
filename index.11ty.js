af99946dda0090554364c8b2eab3fb0dfb5f14d6
An Vo <thien.an.vo.nguyen@gmail.com>
2024-02-28 14:24:12 +0700

    Improve compiling (#1)

---
+++ index.11ty.js
@@ -0,0 +1,20 @@
+exports.data = function () {
+  return {
+    eleventyImport: {
+      collections: ["demos"]
+    },
+  }
+};
+
+const renderReleasedItems = (items) => {
+  return items.map((item) => (`<li>${item}</li>`)).join("\n");
+}
+
+exports.render = function (data) {
+  return `<h1>Our Sprint Track</h1>
+  <ul>
+    ${data.collections.demos.map((sprint) => {
+    return `<li><a href="${sprint.url}">${this.sprintTitle(sprint.content)}</a><br /><ul>${renderReleasedItems(this.releasedItems(sprint.content))}</ul></li>`;
+  }).join("\n")}
+  </ul>`;
+};
